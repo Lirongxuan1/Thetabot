acd66a852f0562f335356517def04ef25efcfda3
Matthew Peng <62450693+pinguchingu@users.noreply.github.com>
2020-08-27 00:49:26 -0700

    main.py better way that uses less memory

--- main.py
+++ main.py
@@ -7,36 +7,34 @@ nasdaq_tickers = si.tickers_nasdaq()
 spy_tickers = si.tickers_sp500()
 
 # replace DOW with DWDP in ticker list
+dow_tickers.remove('DOW')
 
 # scrape the options data for each Dow ticker
-data = pd.read_csv('tickers.csv')
 for ticker in dow_tickers:
     try:
         ticker_data = options.get_options_chain(ticker)
         ticker_df = pd.DataFrame(ticker_data["calls"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "call"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
         ticker_df = pd.DataFrame(ticker_data["puts"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "put"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
     except Exception:
         print(ticker + " failed")
 
 for ticker in nasdaq_tickers:
     try:
         ticker_data = options.get_options_chain(ticker)
-        print(1)
         ticker_df = pd.DataFrame(ticker_data["calls"])
-        print(2)
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "call"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
         ticker_df = pd.DataFrame(ticker_data["puts"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "put"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
     except Exception:
         print(ticker + " failed")
 
@@ -46,23 +44,10 @@ for ticker in spy_tickers:
         ticker_df = pd.DataFrame(ticker_data["calls"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "call"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
         ticker_df = pd.DataFrame(ticker_data["puts"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "put"
-        data = pd.concat([data, ticker_df])
+        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
     except Exception:
         print(ticker + " failed")
-
-
-data.drop_duplicates(inplace=True)
-data.to_csv('tickers.csv', index=False)
-
-# db_out = pd.concat([db, matched_data])
-#chain = options.get_options_chain("nflx")
-#df = pd.DataFrame(chain["calls"])
-#df['Ticker Id'] = "nflx"
-#df['Option Type'] = "call"
-
-#df.to_csv('tickers.csv', index=False)
-# print(df)
