2b3efbc1851b1c2f69e25fc32cfe52466b02e341
Ryan Li <ryanli@usc.edu>
2020-08-31 14:48:02 -0700

    renamed files
    ready for data collection phase

--- fetch_data.py
+++ fetch_data.py
@@ -1,20 +1,20 @@
 from yahoo_fin import options, requests
 import pandas as pd
 import pickle
+from time import gmtime, strftime
 
 tickers = pickle.load(open('list.pkl', 'rb'))
 # scrape the options data for each Dow ticker
-
 for ticker in tickers:
     try:
         ticker_data = options.get_options_chain(ticker)
         ticker_df = pd.DataFrame(ticker_data["calls"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "call"
-        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
+        ticker_df.to_csv('options_data.csv', mode="a", header=False, index=False)
         ticker_df = pd.DataFrame(ticker_data["puts"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "put"
-        ticker_df.to_csv("tickers.csv", mode="a", header=False, index=False)
+        ticker_df.to_csv('options_data.csv', mode="a", header=False, index=False)
     except Exception:
         print(ticker + " failed")
