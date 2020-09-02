908bbd0a7385447c698ed361138a1207f7246ff5
Ryan Li <ryanli@usc.edu>
2020-09-02 15:53:58 -0700

    Change script for data access
    Will save data to current date/time.csv

--- fetch_data.py
+++ fetch_data.py
@@ -1,21 +1,22 @@
 from yahoo_fin import options, requests
 import pandas as pd
 import pickle
-from time import gmtime, strftime
-
+from datetime import datetime
+now = datetime.now()
+date_time = now.strftime("%m-%d-%Y-%H")
 tickers = pickle.load(open('list.pkl', 'rb'))
-# scrape the options data for each Dow ticker
+print(date_time)
 for ticker in tickers:
     try:
         ticker_data = options.get_options_chain(ticker)
         ticker_df = pd.DataFrame(ticker_data["calls"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "call"
-        ticker_df.to_csv('options_data.csv', mode="a", header=False, index=False)
+        ticker_df.to_csv(date_time+ '.csv', mode="a", header=False, index=False)
         ticker_df = pd.DataFrame(ticker_data["puts"])
         ticker_df['Ticker Id'] = ticker
         ticker_df['Option Type'] = "put"
-        ticker_df.to_csv('options_data.csv', mode="a", header=False, index=False)
+        ticker_df.to_csv(date_time + '.csv', mode="a", header=False, index=False)
     except Exception:
         print(ticker + " failed")
 print('done')
